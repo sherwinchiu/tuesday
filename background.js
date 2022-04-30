38a27861ad3896c82da1016dc8efb1cad8c78403
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-04-29 20:55:37 -0400

    commenting good

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
-var menuItem = {
+/**
+ * makes an opion exist when you right click and displays it
+ */
+chrome.contextMenus.create({
     "id": "tuesday-extension-add-to-calendar",
     "title": "Add to Calendar",
     "contexts": ["selection"]
-}
-chrome.contextMenus.create(menuItem);
+});
 
+/**
+ * on clicked listener
+ * checks if the right clicked option is one relevant to our chrome exnteion
+ * tosses the date into the storage and creates a notification saying it got added
+ */
 chrome.contextMenus.onClicked.addListener(function(clicked) {
     if(clicked.menuItemId == "tuesday-extension-add-to-calendar") {
         chrome.storage.sync.set({"date": clicked.selectionText}, function() {
