3f85b26d8bbc97c5826a3a87b37fab10b5699680
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-04-29 23:23:24 -0400

    added keybindings

--- background.js
+++ background.js
@@ -17,9 +17,9 @@ chrome.contextMenus.create({
  * checks if the right clicked option is one relevant to our chrome exnteion
  * tosses the date into the storage and creates a notification saying it got added
  */
-chrome.contextMenus.onClicked.addListener(function(clicked) {
+chrome.contextMenus.onClicked.addListener((clicked) => {
     if(clicked.menuItemId == "tuesday-extension-add-date-to-calendar") {
-        chrome.storage.sync.set({"date": clicked.selectionText}, function() {
+        chrome.storage.sync.set({"date": clicked.selectionText}, () => {
             chrome.notifications.create("added",{
                 type: "basic",
                 iconUrl:"/assets/img/48x48 logo.png",
@@ -28,6 +28,22 @@ chrome.contextMenus.onClicked.addListener(function(clicked) {
             });
         });
     } else if(clicked.menuItemId == "tuesday-extension-add-event-to-calendar") {
-        chrome.tabs.create({url: `https://www.google.com/calendar/render?action=TEMPLATE&text=${clicked.selectionText}`});
+        chrome.storage.sync.set({"event": clicked.selectionText}, () => {
+            chrome.tabs.create({url: `https://www.google.com/calendar/render?action=TEMPLATE&text=${clicked.selectionText}`});
+        });
+    }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command == "add_event") {
+        chrome.storage.sync.get(["event", "date"], (event) => {
+            chrome.tabs.create({url: `https://www.google.com/calendar/render?action=TEMPLATE&text=${event.event}`});
+            chrome.notifications.create("added",{
+                type: "basic",
+                iconUrl:"/assets/img/48x48 logo.png",
+                title: "Added an Event",
+                message: `Added ${event.event}`
+            });
+        });
     }
 });
