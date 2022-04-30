4fb699d6bba9f115188e8b905e3280d5a17bfe58
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-04-30 09:43:52 -0400

    added colour themes

--- assets/js/spin.js
+++ assets/js/spin.js
@@ -5,5 +5,7 @@ chrome.storage.sync.get(["spinSpeed"], (speed) => {
     if (speed != undefined) {
         let root = document.documentElement;
         root.style.setProperty("--spin-duration", `${speed.spinSpeed}s`);
+    } else {
+        chrome.storage.sync.set({"spinSpeed": 0});
     }
 });
--- options.js
+++ options.js
@@ -1,12 +1,46 @@
 var speedSlider = document.getElementById("spin-speed");
+var bgColourOptions = document.getElementById("bg-col");
 
-chrome.storage.sync.get(["spinSpeed"], (speed) => {
-    speedSlider.value = speed.spinSpeed;
+const BG_COLS = {
+    "none": "",
+    "rgb": "rgb(var(--red), var(--green), var(--blue))",
+    "dark": "#333",
+    "light": "#fff"
+}
+
+const TXT_COLS = {
+    "none": "",
+    "rgb": "rgb(var(--inv-red), var(--inv-green), var(--inv-blue))",
+    "dark": "#eee",
+    "light": "#000"
+}
+
+const DECODE_BG = {
+    "": "none",
+    "rgb(var(--red), var(--green), var(--blue))": "rgb",
+    "#333": "dark",
+    "#fff": "light"
+}
+
+chrome.storage.sync.get(["spinSpeed", "bgCol"], (options) => {
+    speedSlider.value = options.spinSpeed;
+    bgColourOptions.value = DECODE_BG[options.bgCol];
 });
 
 document.getElementById("set-options").addEventListener("click", () => {
+    let root = document.documentElement;
+    // spin speed
     chrome.storage.sync.set({"spinSpeed": speedSlider.value}, () => {
-        let root = document.documentElement;
         root.style.setProperty("--spin-duration", `${speedSlider.value}s`);
     });
+
+    // background colour
+    chrome.storage.sync.set({"bgCol": BG_COLS[bgColourOptions.value]}, () => {
+        root.style.setProperty("--background-colour", BG_COLS[bgColourOptions.value]);
+    });
+    // text colour
+    chrome.storage.sync.set({"txtCol": TXT_COLS[bgColourOptions.value]}, () => {
+        root.style.setProperty("--colour", TXT_COLS[bgColourOptions.value]);
+    });
+    
 });
