8ebdeb398c3de42185bb4368e9cc72a6a85d00de
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-04-30 08:42:21 -0400

    fixed spin adjust options

--- options.js
+++ options.js
@@ -1,12 +1,12 @@
 var speedSlider = document.getElementById("spin-speed");
 
-chrome.storage.sync.get("spin-speed", (speed) => {
-    speedSlider.value = speed;
+chrome.storage.sync.get(["spinSpeed"], (speed) => {
+    speedSlider.value = speed.spinSpeed;
 });
 
 document.getElementById("set-options").addEventListener("click", () => {
-    chrome.storage.sync.set({"spin-speed": speedSlider.value}, () => {
+    chrome.storage.sync.set({"spinSpeed": speedSlider.value}, () => {
         let root = document.documentElement;
-        root.style.setProperty("--spin-duration", `${speed}s`);
+        root.style.setProperty("--spin-duration", `${speedSlider.value}s`);
     });
 });
